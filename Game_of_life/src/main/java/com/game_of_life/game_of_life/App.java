90c96c26ad08d79e858fb0d3423ac28d99323e57
flaneurr <crista.kaukinen@helsinki.fi>
2014-09-03 14:51:34 +0300

    nb projekti ja gitignore-tiedosto lisätty

---
+++ Game_of_life/src/main/java/com/game_of_life/game_of_life/App.java
@@ -0,0 +1,13 @@
+package com.game_of_life.game_of_life;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
