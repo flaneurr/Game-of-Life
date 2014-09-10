ef8eb0204c59c64d57d4b5c52ce50ab4f8d4fd31
flaneurr <crista.kaukinen@helsinki.fi>
2014-09-10 12:04:20 +0300

    koodaus jatkuu

--- Game_of_life/src/main/java/Logiikka/Ruudukko.java
+++ Game_of_life/src/main/java/Logiikka/Ruudukko.java
@@ -7,6 +7,8 @@
 package Logiikka;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 /**
  *
@@ -14,12 +16,53 @@ import java.util.ArrayList;
  */
 public class Ruudukko {
     
-    private ArrayList<Solu> solut;
+    private Solu[][] solut;
     
+    
+    public Ruudukko (){ // todennäköisesti tulee useita konstruktoreja
+        
+        solut = new Solu[20][20];
+        Random rand = new Random();
+ 
+        for (int i = 0; i < solut.length ; i++){
+            for (int j = 0 ; j < solut[i].length ; j++){
+                int tila = rand.nextInt(2); // arvotaan alussa solu joko eläväksi tai kuolleeksi'
+                Solu solu = new Solu(i,j,tila);
+                solut[i][j] = solu;
+            }
+        }
+    }
+    
+    public Solu getSolu(int x, int y){
+        return this.solut[x][y];       
+    }
     
     public void paivitaTilat(){
         // kutsutaan säännön päivitysmetodia, joka päivittää jokaisen solu tilan kerrallaan - muista aika-askleleet!
+        Solu[][] edellinenRuudukko = solut.clone();
+        ArrayList<Integer> syntyma = new ArrayList<Integer>();
+        syntyma.add(2);
+        ArrayList<Integer> selviaminen = new ArrayList<Integer>();
+        selviaminen.add(2);
+        selviaminen.add(3);
         
+        Saanto saanto = new Saanto(syntyma, selviaminen);
+        
+        for (int i = 0; i < solut.length ; i++){
+            for (int j = 0 ; j < solut[i].length ; j++){
+                Solu solu = getSolu(i,j);
+                solu.setTila(saanto.seuraavaTila(solu.getTila(), solu.montakoElavaaNaapuria()));
+            }
+        }
+    }
+    
+    public void tulostaRuudukko(){
+        for (int i = 0; i < solut.length ; i++){
+            for (int j = 0 ; j < solut[i].length ; j++){
+                System.out.print(Integer.toString(getSolu(i,j).getTila()));
+            }
+            System.out.println("");
+        }
     }
     
 }
--- Game_of_life/src/main/java/Logiikka/Saanto.java
+++ Game_of_life/src/main/java/Logiikka/Saanto.java
@@ -22,6 +22,17 @@ public class Saanto {
         this.selviaminen = new ArrayList<Integer>();
     }
     
+    public int seuraavaTila(int edellinenTila, int naapurienLkm){
+        int seuraavaTila = 0;
+        if (edellinenTila == 1 && selviaminen.contains(naapurienLkm)){
+            seuraavaTila = 1;
+        } else if (edellinenTila == 0 && syntyma.contains(naapurienLkm)){
+            seuraavaTila = 1;
+        }
+        return seuraavaTila;
+        
+    }
+    
     public void setSyntyma(ArrayList<Integer> syntyma){
         this.syntyma = syntyma;
     }
