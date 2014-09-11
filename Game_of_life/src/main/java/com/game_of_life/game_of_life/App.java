fdd194cc45828618fe8a3159d1f48b596249fa93
flaneurr <crista.kaukinen@helsinki.fi>
2014-09-11 19:19:30 +0300

    kaikki lisätty

--- Game_of_life/src/main/java/com/game_of_life/game_of_life/App.java
+++ Game_of_life/src/main/java/com/game_of_life/game_of_life/App.java
@@ -1,5 +1,8 @@
 package com.game_of_life.game_of_life;
 
+import Logiikka.Ruudukko;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Ruudukko ruudukko = new Ruudukko();
+        ruudukko.tulostaRuudukko();
+        
+//        ArrayList<Integer> syntyma = new ArrayList<>();
+//        syntyma.add(3);
+//        ArrayList<Integer> selviaminen = new ArrayList<>();
+//        selviaminen.add(2);
+//        selviaminen.add(3);
+//        
+//        ruudukko.luoSaanto(syntyma, selviaminen);
+        
+        System.out.println("");
+        ruudukko.paivitaRuudukko();
+        ruudukko.tulostaRuudukko();
     }
 }
