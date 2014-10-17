d3292832544b488aeccc479c3ba5b5de0495b7fb
Kaukinen Crista C C <crkaukin@da1-cs-bk107-27.helsinki.fi>
2014-10-17 11:20:13 +0300

    javadockia lisäilty

--- Game_of_life/src/main/java/Logiikka/Kuvio.java
+++ Game_of_life/src/main/java/Logiikka/Kuvio.java
@@ -4,26 +4,44 @@ package Logiikka;
 import java.awt.Color;
 import java.util.ArrayList;
 /**
- * Abstahoi Game of Life-pelissä esiintyvät yhtenäiset kuviot. Sisältää kuvioon kuuluvat solut ja oman värinsä.
+ * Abstahoi Game of Life-pelissä esiintyvät yhtenäiset kuviot, jotka koostuvat vierekkäisistä elossa olevista soluista.
+ * Luokka sisältää kuvioon kuuluvat solut ja oman värinsä.
  * @author Kisuli
  */
 public class Kuvio {
     
+    /**
+     * Lista kuvioon kuuluvista soluista.
+     */
     private ArrayList<Solu> solut;
+    /**
+     * Kuvion väri. Kuvioon kuuluvat solut saavat värinsä kuviolta.
+     */
     private Color vari;
     
+    /**
+     * Konstruktori, joka luo uuden kuvion, joka on parametrinä olevan värin värinen.
+     * 
+     * @param vari kuvion väri
+     */
     public Kuvio(Color vari){
         this.solut = new ArrayList<>();
         this.vari = vari;
     }
     
+    /**
+     * Luo uuden kuvion. Kuvio on parametrinä olevan värin värinen ja siihen kuuluu parametrinä saadut solut.
+     * 
+     * @param vari kuvion väri
+     * @param solut kuvioon kuuluvat solut
+     */
     public Kuvio(Color vari, ArrayList<Solu> solut){
         this.solut = solut;
         this.vari = vari;
     }
     /**
      * Asettaa kuviolle värin.
-     * @param vari 
+     * @param vari kuvion uusi väri
      */
     public void setVari(Color vari){
         this.vari = vari;
